4cbcf4466cf5b93763764482fd3fcd58fd822713
Victor Song <victor.song.02@gmail.com>
2018-06-21 01:17:04 +0200

    Added pagination, it's DONE

--- src/Main/main.java
+++ src/Main/main.java
@@ -22,7 +22,7 @@ public class main {
     }
 
     private static File toFile(String url, String path) {
-        File file = new File(path + url.substring(url.lastIndexOf("/") + 1, url.length()));
+        File file = new File(path);
         if (!file.exists()) {
             try {
                 FileUtils.copyURLToFile(new URL(url), file);
@@ -33,26 +33,27 @@ public class main {
         return file;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String username = in.nextLine();
         in.close();
 
         JsonParser parser = new JsonParser();
         JsonArray favourites = null;
-        try {
-            favourites = (JsonArray) parser.parse(toString("https://api.soundcloud.com/users/" + username + "/favorites?client_id=e7pCwTm0KONbBx2NoD5a0k3kP474wQnC"));
-        } catch (IOException e) {
-            System.out.println("bad user id");
-        }
-        for (int i = 0; i < favourites.size(); i++) {
-            JsonObject favourite = (JsonObject) favourites.get(i);
-            String title = favourite.get("title").toString().replace("\"", "");
-            String url = favourite.get("stream_url").toString().replace("\"", "");
-
-            Document favurl = Jsoup.connect(url).get();
-            String dlink = favurl.getElementsByTag("source").get(0).attr("src");
-            toFile(dlink, title + ".mp3");
+        for (int page = 0; ; page += 50) {
+            try {
+                favourites = (JsonArray) parser.parse(toString("https://api.soundcloud.com/users/" + username + "/favorites?client_id=e7pCwTm0KONbBx2NoD5a0k3kP474wQnC&offset=" + page));
+            } catch (IOException e) {
+                System.out.println("bad user id");
+            }
+            for (int i = 0; i < favourites.size(); i++) {
+                JsonObject favourite = (JsonObject) favourites.get(i);
+                String title = favourite.get("title").toString().replace("\"", "");
+                String url = favourite.get("stream_url").toString().replace("\"", "");
+                toFile(url + "?client_id=e7pCwTm0KONbBx2NoD5a0k3kP474wQnC", "songs/" + title + ".mp3");
+                System.out.println(title);
+            }
+            if (favourites.size() < 50) break;
         }
     }
 }
